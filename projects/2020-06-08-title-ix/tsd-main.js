9626108004c02a7508db726226064f4ccb9ec91a
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-06-07 11:03:20 -0400

    fix more stuff

---
+++ projects/2020-06-08-title-ix/tsd-main.js
@@ -0,0 +1,21 @@
+$(function() {
+  // var elements = [];
+  // $(".tsd-callout").each(function() {
+  //   elements.push({
+  //     element: this,
+  //     top: this.getBoundingClientRect().top,
+  //     bottom: this.getBoundingClientRect().bottom
+  //   });
+  // });
+  // console.log(elements);
+  // $(document).scroll(function() {
+  //   for (let i = elements.length - 1; i >= 0; i--) {
+  //     var element = elements[i];
+  //     if (window.scrollY > element.bottom && elements[i + 1] && window.scrollY < elements[i + 1].top) { // next element.top
+  //       console.log(window.scrollY, element.bottom);
+  //       elements[i + 1].element.scrollIntoView({  });
+  //       break;
+  //     }
+  //   }
+  // });
+});
