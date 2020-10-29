cc6ab119ee0f8e4a82200742bf37ba4ae196f8ff
Arjun Ramani <arjun@Arjuns-MacBook-Pro-2.local>
2020-10-28 23:34:48 -0700

    fix script to only CA

--- projects/2020-10-bay-area-covid/bay_area_cases_deaths.py
+++ projects/2020-10-bay-area-covid/bay_area_cases_deaths.py
@@ -7,7 +7,7 @@ df = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master
 counties = ['Alameda', 'Contra Costa', 'Marin', 'Monterey', 'Napa', 'San Francisco',
            'San Joaquin', 'San Mateo', 'Santa Clara', 'Santa Cruz', 'Solano', 'Sonoma']
 
-df = df[np.isin(df['county'],counties)]
+df = df[(np.isin(df['county'],counties)) & (df['state'] == 'California')]
 
 df.sort_values(by = ['county', 'date'], inplace = True)
 
