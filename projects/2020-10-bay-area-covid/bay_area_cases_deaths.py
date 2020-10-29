4836650c5ae630ebef1ca054b62cf7ca49b5a00e
Arjun Ramani <arjun@Arjuns-MacBook-Pro-2.local>
2020-10-28 18:25:40 -0700

    create bay area covid script

---
+++ projects/2020-10-bay-area-covid/bay_area_cases_deaths.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv',
+                sep = ',')
+
+counties = ['Alameda', 'Contra Costa', 'Marin', 'Monterey', 'Napa', 'San Francisco',
+           'San Joaquin', 'San Mateo', 'Santa Clara', 'Santa Cruz', 'Solano', 'Sonoma']
+
+df = df[np.isin(df['county'],counties)]
+
+df.sort_values(by = ['county', 'date'], inplace = True)
+
+df.to_csv('bay_area_cases_deaths_clean.csv')
