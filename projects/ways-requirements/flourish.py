e8d368523ce9509019d5a334898862368d936c5a
Matthew Turk <mjturk@stanford.edu>
2024-04-22 19:51:39 -0700

    Minor refactoring

--- projects/ways-requirements/flourish.py
+++ projects/ways-requirements/flourish.py
@@ -70,6 +70,21 @@ Chords between departments to see how strong cross listing for certain ones that
 Ratio number of units for major against number of ways requirements that major satisfies
 """
 
+def scatter_plot(start: int, stop: int) -> pd.DataFrame:
+    data = []
+
+    for year in years_list(start, stop):
+        df = glob_json(f"/Users/matthewturk/Desktop/ways-data/{year}")
+        has_sections = df["sections"].apply(lambda x: len(x) > 0)
+        req = df["gers"].apply(lambda r: any(g.startswith("WAY-") for g in r))
+        df = df[has_sections & req]
+        row = dict(df["gers"].explode().value_counts())
+        row = {key: value for key, value in row.items() if "WAY-" in key}
+        row["Year"] = year
+        data.append(row)
+
+    return pd.DataFrame(data)
+
 if __name__ == "__main__":
     # department_codes = ["EARTHSYS", "ENERGY", "ENVRES", "EPS", "ESS", "OCEANS", "GEOPHYS", "SUSTAIN", "SUST", "ACCT", "ALP", "BUSGEN", "MGTECON", "FINANCE", "GSBGEN", "GSBGID", "HRMGT", "MKTG", "OIT", "OB", "POLECON", "STRAMGT", "EDUC", "AA", "BIOE", "CHEMENG", "CEE", "CME", "CS", "DESIGN", "DESINST", "EE", "ENGR", "MS&E", "MATSCI", "ME", "SCCM", "AFRICAAM", "AMELANG", "AFRICAST", "AMSTUD", "ANTHRO", "APPPHYS", "ARABLANG", "ARCHLGY", "ARTHIST", "ARTSINST", "ARTSTUDI", "ASNAMST", "ASNLANG", "BIO", "BIOHOPK", "BIOPHYS", "CATLANG", "CHEM", "CHILATST", "CHINA", "CHINLANG", "CLASSICS", "COMM", "COMPLIT", "CSRE", "DANCE", "DATASCI", "DLCL", "TAPS", "EALC", "EASTASN", "ECON", "ENGLISH", "EFSLANG", "ETHICSOC", "FEMGEN", "FILMPROD", "FILMEDIA", "FRENLANG", "FRENCH", "GERLANG", "GERMAN", "GLOBAL", "HISTORY", "HPS", "HUMBIO", "HUMRTS", "HUMCORE", "HUMSCI", "ILAC", "IIS", "INTLPOL", "INTNLREL", "ITALLANG", "ITALIAN", "JAPAN", "JAPANLNG", "JEWISHST", "KOREA", "KORLANG", "LATINAM", "LINGUIST", "MLA", "MCS", "MATH", "MEDVLST", "MTL", "MUSIC", "NATIVEAM", "PHIL", "PHYSICS", "POLISCI", "PORTLANG", "PSYCH", "PUBLPOL", "RELIGST", "REES", "STS", "SLAVLANG", "SLAVIC", "SOC", "SPANLANG", "ILAC", "SPECLANG", "SIW", "STATS", "SYMSYS", "TAPS", "TIBETLNG", "URBANST", "LAW", "LAWGEN", "ANES", "BIOC", "BIODS", "BIOMEDIN", "BMP", "BIOS", "CBIO", "CTS", "CSB", "CHPR", "COMPMED", "DERM", "DBIO", "EMED", "EPI", "FAMMED", "GENE", "HRP", "IMMUNOL", "LEAD", "LIFE", "MED", "INDE", "MI", "MCP", "NBIO", "NENS", "NEPR", "NSUR", "OBGYN", "OPHT", "ORTHO", "OTOHNS", "PATH", "PEDS", "PAS", "PSYC", "RADO", "RAD", "SOMGEN", "STEMREM", "SBIO", "SURG", "UROL", "WELLNESS", "CTL", "COLLEGE", "ESF", "ITALIC", "SOAR", "ORALCOMM", "OSPGEN", "OSPAUSTL", "OSPBARCL", "OSPBEIJ", "OSPBER", "OSPCPTWN", "OSPFLOR", "OSPHONGK", "OSPISTAN", "OSPKYOTO", "OSPKYOCT", "OSPMADRD", "OSPOXFRD", "OSPPARIS", "OSPSANTG", "RESPROG", "ROTCAF", "ROTCARMY", "ROTCNAVY", "SINY", "SLE", "THINK", "UAR", "PWR", "VPTL"]
     # write_time_major_composition(2014, 2024, department_codes)
@@ -77,17 +92,21 @@ if __name__ == "__main__":
     # print(ways_value_counts("2023-2024", "HISTORY", drop_duplicates=True))
     df = glob_json("/Users/matthewturk/Desktop/ways-data/2023-2024")
     df = df.dropna(subset=["gers"])
-
+    has_sections = df["sections"].apply(lambda x: len(x) > 0)
     subject = df["subject"] == "HISTORY"
     # req = df["gers"].apply(lambda x: "WAY-CE" in x) # df["gers"].astype(str).str.contains("WAY-CE")
     # df = df.drop_duplicates(subset=["course_id"])
-    req = df["gers"].astype(str).str.contains("WAY")
-    has_sections = df["sections"].apply(lambda x: len(x) > 0)
+    req = df["gers"].apply(lambda r: any(g.startswith("WAY-") for g in r)) # df["gers"].astype(str).str.contains("WAY")
 
     df = df[has_sections & req]
     # df = df[subject & req]
     # df = df["sections"]
-    print(df["gers"])
+    # 1,876 courses in 2023-24 school year satisfy ways.
+    # if you account for cross listings, that number is then 1,266.
+    row = dict(df["gers"].explode().value_counts())
+    row = {key: value for key, value in row.items() if "WAY-" in key}
+    # print(row)
     # for row in df["sections"]:
     #     if len(row) > 0:
     #         print(row)
+    print(scatter_plot(2014, 2024))
